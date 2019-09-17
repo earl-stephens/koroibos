e7f0770686022c52c6db68eb599e4dda4fa63d36
earl-stephens <stephensearl@hotmail.com>
2019-09-17 13:44:53 -0600

    Finishing testing and coding for finding oldest athlete

--- spec/requests/get_all_olympians_spec.rb
+++ spec/requests/get_all_olympians_spec.rb
@@ -67,4 +67,37 @@ RSpec.describe 'get all olympians', type: :request do
     expect(results[0]).to have_key("total_medals_won")
     expect(results[0]["total_medals_won"]).to eq(0)
   end
+
+  it 'returns the oldest olympian' do
+    athlete1 = Athlete.create!(name: 'Kermit', age: 21, sex: 'M', height: 30, weight: 5, team: 'USA')
+    athlete2 = Athlete.create!(name: 'Piggy', age: 28, sex: 'F', height: 30, weight: 15, team: 'USA')
+    athlete3 = Athlete.create!(name: 'Fozzy', age: 22, sex: 'M', height: 55, weight: 25, team: 'USA')
+    athlete4 = Athlete.create!(name: 'Gonzo', age: 20, sex: 'M', height: 33, weight: 9, team: 'USA')
+    sport1 = Sport.create!(name: 'Wrestling')
+    sport2 = Sport.create!(name: 'Lumberjack')
+    sport3 = Sport.create!(name: 'Running')
+    event1 = sport1.events.create!(name: 'Mud Wrestling', games: '2016')
+    event2 = sport2.events.create!(name: 'Axe Throwing', games: '2016')
+    event3 = sport3.events.create!(name: 'Marathon', games: '2016')
+    ae1 = event1.athlete_events.create!(medals: 'Silver', athlete_id: athlete1.id)
+    ae2 = event2.athlete_events.create!(medals: 'Bronze', athlete_id: athlete2.id)
+    ae3 = event3.athlete_events.create!(medals: 'Gold', athlete_id: athlete3.id)
+    ae4 = event1.athlete_events.create!(medals: 'NA', athlete_id: athlete4.id)
+
+    get '/api/v1/olympians?age=oldest'
+
+    results = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(results[0]).to have_key("name")
+    expect(results[0]["name"]).to eq(athlete2.name)
+    expect(results[0]).to have_key("team")
+    expect(results[0]["team"]).to eq(athlete2.team)
+    expect(results[0]).to have_key("age")
+    expect(results[0]["age"]).to eq(athlete2.age)
+    expect(results[0]).to have_key("sport")
+    expect(results[0]["sport"]).to eq(sport2.name)
+    expect(results[0]).to have_key("total_medals_won")
+    expect(results[0]["total_medals_won"]).to eq(1)
+  end
 end
